ee1eee6da0aaca37aee60eee0b4623a2d7a1b64d
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-11 11:30:04 -0400

    Alternate Version

--- kyu_7/dna_strand.py
+++ kyu_7/dna_strand.py
@@ -5,3 +5,6 @@
 def dna_strand(dna):
     reverse = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join(reverse[x] for x in dna)
+
+# def dna_strand(dna):
+#     return dna.translate(str.maketrans("ATGC", "TACG"))
