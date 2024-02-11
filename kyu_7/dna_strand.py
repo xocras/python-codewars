2bd6f824a1ae04f8c7ad9d950c5eccc1764a0cd1
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-11 11:06:20 -0400

    Uploaded Kata

---
+++ kyu_7/dna_strand.py
@@ -0,0 +1,7 @@
+# Complementary DNA
+
+# https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
+
+def dna_strand(dna):
+    reverse = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    return ''.join(reverse[x] for x in dna)
