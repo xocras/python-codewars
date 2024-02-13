87ba292485f4bcd1d749b166a4f6439b5a106e88
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-12 21:55:06 -0400

    Updated Kata

--- kyu_7/get_sum.py
+++ kyu_7/get_sum.py
@@ -2,8 +2,8 @@
 
 # https://www.codewars.com/kata/55f2b110f61eb01779000053/train/python
 
-def get_sum(a,b):
+def get_sum(a, b):
     return sum(range(b, a - 1, -1) if a < b else range(a, b - 1, -1))
 
-# def get_sum(a,b):
+# def get_sum(a, b):
 #     return sum(range(min(a, b), max(a, b) + 1))
