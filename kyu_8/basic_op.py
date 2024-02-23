b3f1872bb72a2a4af556860540c3265438b6d6ce
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-23 01:24:24 -0400

    Updated Kata

---
+++ kyu_8/basic_op.py
@@ -0,0 +1,6 @@
+# Basic Mathematical Operations
+
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/python
+
+def basic_op(operator, value1, value2):
+    return eval(f'{value1}{operator}{value2}')
