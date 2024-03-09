7ca4610485519314df40b129ce0fff403a3339de
Oscar Cruz <oscarfcr.works@gmail.com>
2024-03-09 14:22:00 -0400

    Updated Kata

--- kyu_6/Is a number prime/main.py
+++ kyu_6/Is a number prime/main.py
@@ -3,11 +3,11 @@
 # https://www.codewars.com/kata/5262119038c0985a5b00029f/train/python
 
 def is_prime(num):
-    if num < 2 or (not num % 2 and num != 2):
+    if num < 2 or (num % 2 == 0 and num != 2):
         return False
 
-    for n in range(3, int(num ** 0.5) + 1, 2):
-        if not num % n:
+    for divisor in range(3, int(num ** 0.5) + 1, 2):
+        if num % divisor == 0:
             return False
 
     return True
