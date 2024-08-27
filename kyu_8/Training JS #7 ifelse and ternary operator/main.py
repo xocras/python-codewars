8a8b8555805ccf2f4a88e83257271206dc44df4d
Oscar Cruz <oscarfcr.works@gmail.com>
2024-08-26 22:03:55 -0400

    Updated Kata

---
+++ kyu_8/Training JS #7 ifelse and ternary operator/main.py
@@ -0,0 +1,7 @@
+# Training JS #7: if..else and ternary operator
+
+# https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/python
+
+def sale_hotdogs(n):
+    return n * (100 if n < 5 else 95 if n < 10 else 90)
+
