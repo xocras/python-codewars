368ee60e8cde35c3504f13f12492d730d0e5e26a
Oscar Cruz <oscarfcr.works@gmail.com>
2024-08-31 14:12:03 -0400

    Updated Kata

---
+++ kyu_7/Money, Money, Money/main.py
@@ -0,0 +1,12 @@
+# Money, Money, Money
+
+# https://www.codewars.com/kata/563f037412e5ada593000114/train/python
+
+def calculate_years(principal, interest, tax, desired):
+    years = 0
+    while principal < desired:
+        principal *= (1 + interest * (1 - tax))
+        years += 1 
+    return years
+
+
