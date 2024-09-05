3ebb53485c3b1ef331960a7b50d6e1389c404c6c
Oscar Cruz <oscarfcr.works@gmail.com>
2024-09-05 03:55:51 -0400

    Updated Kata

---
+++ kyu_8/Grasshopper - Ifelse syntax debug/main.py
@@ -0,0 +1,6 @@
+# Grasshopper - If/else syntax debug
+
+# https://www.codewars.com/kata/57089707fe2d01529f00024a/train/python
+
+def check_alive(health):
+    return health > 0
---
+++ kyu_8/Stringy Strings/main.py
@@ -0,0 +1,6 @@
+# Stringy Strings
+
+# https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/python
+
+def stringy(size):
+    return ('10' * size)[:size]
